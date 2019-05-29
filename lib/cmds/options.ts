099baec71cc2a239bfcf7cf04cb781e1c2bf4ec7
Filip Zamorsky <filipzamorsky@gmail.com>
2019-05-29 12:52:16 +0200

    Update options.ts

--- lib/cmds/options.ts
+++ lib/cmds/options.ts
@@ -44,7 +44,7 @@ export interface Server {
   // Detach the server and return the process to the parent.
   runAsDetach?: boolean;
   // Run as selenium grid = node option.
-  runAsGrid?: boolean;
+  runAsGrid?: string;
   // Port number to start the server.
   port?: number;
 }
